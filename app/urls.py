496c0e147bbcb7cb4314e9a4b627084d6fd04554
Tsuyoshi Takebayashi <s1f101703218@iniad.org>
2019-12-11 15:01:08 +0900

    フロントだけ動くやつ
    フロントだけ動くやつです

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .views import SignUpView,  post_create, user_detail, user_prof_update,SignUpCompView
+from .views import SignUpView,  post_create, user_detail, user_prof_update,SignUpCompView,HomeView
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('new/', SignUpView.as_view(),name='new'),
     path('complete/', SignUpCompView.as_view(), name='complete'),
     #[develop branch]-added
-    path('index/', post_create, name='index'),#TimeLineAndPostView or post_create
+    path('login/', views.SignUpView.as_view(), name='login'),#ログイン画面
+    path('home/',HomeView.as_view(),name='home'),#home画面
     path('detail/', user_detail, name='detail'),#ユーザーページ
     path('profupdt/', user_prof_update, name='profupdt')#プロフ更新ページ
 ]
