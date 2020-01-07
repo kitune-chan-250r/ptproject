88f7886126efdf7240bb6e432b53de38ab4c5ffb
kitune-chan-250r <nekoo.hisui@gmail.com>
2020-01-07 15:07:12 +0900

    save

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import User, Post, Prof
-
-# Register your models here.
-"""
-
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Prof)
-"""
